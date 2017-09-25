22b6dae1517058ca27578c0d0d38b0fb8459ef1b
Josephina Bos <josephinabos@gmail.com>
2017-09-25 14:44:42 +0200

    set up database

--- outreachcampaign/contents/models.py
+++ outreachcampaign/contents/models.py
@@ -3,4 +3,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.utils import timezone
+
 # Create your models here.
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=200)
+    company_name = models.CharField(max_length=200)
+    website = models.CharField(max_length=200)
+    pitch = models.TextField()
+    consultant_name = models.CharField(max_length=200)
+    consultant_avatar = models.CharField(max_length=200)
+    date = models.DateTimeField(default=timezone.now)
+
+
